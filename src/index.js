a45b9a6f1ef92a003abaa003ac5eb3a69c5b0116
Aashin A K <aashin9605@gmail.com>
2024-06-26 16:25:26 +0530

    create Api error and response handler

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import connectDb from "./db/index.js";
 const port = 4000 || process.env.PORT
 connectDb()
     .then(() => { 
+        app.on("error",(err) => console.log("Error: ",err))
         app.listen(port, () => {
-            console.log(`Server running at port ${port}`);
+            console.log(`⚙️  Server running at port ${port}`);
         })
     })
